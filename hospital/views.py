b0605d9b320ecfee237d505842e3509a1510aff5
quirrk <nicolas.r.suarez@gmail.com>
2019-06-16 16:44:56 -0400

    implementacion frontend

--- hospital/views.py
+++ hospital/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'hospital/index.html', {})
+
+def conocenos(request):
+    return render(request, 'hospital/conocenos.html', {})
