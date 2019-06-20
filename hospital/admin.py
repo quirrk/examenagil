f25cf801244fbc5650d34e937ae68ca30abbf902
Felipe Herrera <felipe.ah14@gmail.com>
2019-06-19 21:17:26 -0400

    cosillas

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from hospital.models import Paciente, Secretaria, Medico
+from hospital.models import Paciente, Medico
 # Register your models here.
 admin.site.register(Paciente)
 admin.site.register(Medico)
